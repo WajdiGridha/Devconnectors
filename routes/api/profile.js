ccf8b2e3a0e1ac59ff7e0b91d2cebe8f3cab5308
wajdi gr <wajdigridha744@gmail.com>
2022-02-03 19:35:31 +0100

    Final Project

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route GET api/profile/me
 // @desc  Get current users profile
@@ -145,7 +146,8 @@ router.get('/user/:user_id', async (req, res)=> {
 // @access Private
 router.delete('/', auth ,async (req, res)=> {
     try {
-        //@todo - remove users posts
+        // Remove user posts
+        await Post.deleteMany({ user: req.user.id })
         // Remove profile
         await Profile.findOneAndRemove({ user: req.user.id});
         // Remove user
